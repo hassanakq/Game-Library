162af03fc905d8e760b2ce88a3e5436f86571a1b
HASSAN QAFISHEH <108220255+hassanakq@users.noreply.github.com>
2026-09-12 19:04:20 +0300

    Change 'py' to 'python' in build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
 @echo off
-REM Build the GameLibrary.exe using the spec file (which bundles pygame /
-REM pywebview's native files correctly -- this is what fixes the controller).
-py -m PyInstaller --noconfirm --clean GameLibrary.spec
+python -m PyInstaller --noconfirm --clean GameLibrary.spec
